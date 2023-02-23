389734a77e0e49a4fd7ce766e1a7d85b4993c73c
itimbalistru <itimbalistru@amsoft-group.com>
2023-02-23 11:51:33 +0200

    Homework 7

---
+++ automation-course/src/topic2/java/Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main(String[] args) {
+
+        Persoana marinel = new Persoana("Marin", 20, 'M');
+        System.out.println("Nume: " + marinel.nume);
+        System.out.println("Varsta: " + marinel.getVarsta());
+        System.out.println("Sex: " + marinel.sex);
+
+    }
+}
---
+++ automation-course/src/topic2/java/Persoana.java
@@ -0,0 +1,26 @@
+public class Persoana {
+
+    public String nume;
+
+    private Integer varsta;
+
+    protected Character sex;
+
+    public Persoana(String nume, int varsta, char sex) {
+        this.nume = nume;
+        this.varsta = varsta;
+        this.sex = sex;
+
+    }
+
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+
+}
