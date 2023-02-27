979b382dc54f199cd5a5e86dae353d75070c866d
itimbalistru <itimbalistru@amsoft-group.com>
2023-02-27 09:13:53 +0200

    Code design

--- automation-course/src/main/topic3/Operations.java
+++ automation-course/src/main/topic3/Operations.java
@@ -1,12 +1,13 @@
 public class Operations {
     public static void printAritmeticOperations() {
-        int a = 10, b = 5;
+        int a = 10, b = 4;
         int sum = a + b;
         int sub = a - b;
         int mul = a * b;
         int div = a / b;
         int quotient = a % b;
-        System.out.println("Arithmetic operations:");
+
+        System.out.println("\n" + "Arithmetic operations:");
         System.out.println("Sum = " + sum);
         System.out.println("Subtraction = " + sub);
         System.out.println("Multiplication = " + mul);
@@ -17,8 +18,8 @@ public class Operations {
     public static void printLogicOperations() {
         boolean b1 = true;
         boolean b2 = false;
-        System.out.println();
-        System.out.println("Logic operations:");
+
+        System.out.println("\n" + "Logic operations:");
         System.out.println("AND = " + (b1 && b2));
         System.out.println("OR = " + (b1 || b2));
         System.out.println("NOT = " + (!b2));
@@ -28,8 +29,7 @@ public class Operations {
         int firstNumber = 10;
         int secondNumber = 5;
 
-        System.out.println();
-        System.out.println("Relational Operations: ");
+        System.out.println("\n" + "Relational Operations: ");
         System.out.println("Equal to: " + (firstNumber == secondNumber));
         System.out.println("Not Equal to: " + (firstNumber != secondNumber));
         System.out.println("Greater than: " + (firstNumber > secondNumber));
@@ -40,16 +40,14 @@ public class Operations {
 
     public static void useForAndWhile() {
         int i = 0;
-        System.out.println();
-        System.out.println("WHILE:");
-        while (i < 4) {
+        System.out.println("\n" + "WHILE:");
+        while (i < 10) {
             System.out.println("Using while loop: " + i);
             i++;
         }
 
-        System.out.println();
-        System.out.println("FOR:");
-        for (int j = 0; j < 6; j++) {
+        System.out.println("\n" + "FOR:");
+        for (int j = 0; j < 10; j++) {
             System.out.println("Using for loop: " + j);
         }
     }
